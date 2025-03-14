2ff97c17bfd6972a0b9e7b907cc451615174ad97
Het <awesomehet1@gmail.com>
2025-03-14 14:12:05 +0530

    second commit - loader issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import { Home, About, Projects, Contact } from './pages'
-import './index.css'
+import { Route, Routes } from 'react-router-dom';
+import { Home, About, Projects, Contact } from './pages';
+import './index.css';
 import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <main className="bg-slate-300/20">
-        <Router>
-          <Navbar />
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
